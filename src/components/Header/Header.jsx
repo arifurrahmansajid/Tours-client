01c537aa735eff969ca9283e5554dbfad908306a
arifurrahmansajid <iamnobody3456@gmail.com>
2024-07-11 15:38:21 +0600

    header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import {Container, Row,Button} from 'reactstrap'
+import {Container, Row, Button} from 'reactstrap'
 import {NavLink, Link} from 'react-router-dom'
-import logo from '../../assets/images/logo.png'
+import Logo from '../../assets/images/logo.png'
 import './header.css'
 const nav__links=[
   {
@@ -22,39 +22,51 @@ const nav__links=[
 ]
 
 const Header = () => {
-  return <header className='header'>
-    <Container>
-      <Row>
-        <div className="nav__wrapper d-flex align-items-center justify-content-between">
+  return (
+     <header className='header' >
+         <Container>
+            <Row>
+               <div className="nav__wrapper d-flex align-items-center justify-content-between">
+                  {/* ========== LOGO ========== */}
+                  <div className="logo">
+                     <img src={Logo} alt="" />
+                  </div>
+                  {/* ========================== */}
 
-              <div className="logo">
-                <img src={logo}alt="" />
-              </div>
+                  {/* ========== MENU START ========== */}
+                  <div className="navigation" >
+                     <ul className="menu d-flex align-items-center gap-5">
+                        {
+                           nav__links.map((item, index) => (
+                              <li className="nav__item" key={index}>
+                                 <NavLink to={item.path} className={navClass => navClass.isActive ? 'active__link' : ''}>{item.display}</NavLink>
+                              </li>
+                           ))
+                        }
+                     </ul>
+                  </div>
+                  {/* ================================ */}
 
-              <div className="navigaation">
-                <ul className="menu d-flex align-items-center gap-5">
-                  {nav__links.map((item, index)=> (
-                    <li className="nav__item" key={index}>
-                      <NavLink to={item.path}>{item.display}</NavLink>
-                    </li>
-                  ))
+                  <div className="nav__right d-flex align-items-center gap-4">
+                     <div className="nav__btns d-flex align-items-center gap-2">
+                        {
+                             <>
+                                 <Button className='btn secondary__btn'><Link to='/login'>Login</Link></Button>
+                                 <Button className='btn primary__btn'><Link to='/register'>Register</Link></Button>
+                              </>
+                        }
+                        
+                     </div>
 
-                  }
-                </ul>
-              </div>
-              <div className="nav__right d-flex align-items-center gap-4">
-                <div className="nav__btns d-flex align-items-center gap-4">
-                  <Button className="btn secondary__btn"><Link typeof='/login'>Login</Link> </Button>
-                  <Button className="btn secondary__btn"><Link typeof='/register'>Register</Link> </Button>
-                </div>
-              <span className="mobile__menu">
-              <i class="ri-menu-line"></i>
-              </span>
-              </div>
-        </div>
-      </Row>
-    </Container>
-  </header>
+                     <span className="mobile__menu" >
+                        <i class="ri-menu-line"></i>
+                     </span>
+                  </div>
+               </div>
+            </Row>
+         </Container>
+      </header>
+  ) 
 };
 
 export default Header;
