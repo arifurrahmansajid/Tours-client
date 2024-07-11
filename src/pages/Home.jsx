4a4c1eefb260728490b463cf5ef3389b56bdb262
arifurrahmansajid <iamnobody3456@gmail.com>
2024-07-11 17:55:04 +0600

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,56 @@
 import React from 'react';
+import '../style/home.css'
+import { Container, Row, Col, CardSubtitle } from 'reactstrap'
+import heroImg from '../assets/images/hero-img01.jpg'
+import heroImg02 from '../assets/images/hero-img02.jpg'
+import heroVideo from '../assets/images/hero-video.mp4'
+import worldImg from '../assets/images/world.png'
+import experienceImg from '../assets/images/experience.png'
+import Subtitle from './../shared/subtitle'
+import Bar from './../shared/Bar';
+
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <section>
+    <Container>
+       <Row>
+          <Col lg='6'>
+             <div className="hero__content">
+                <div className="hero__subtitle d-flex align-items-center">
+                   <Subtitle subtitle={'Know Before You Go'} />
+                   <img src={worldImg} alt="" />
+                </div>
+                <h1>Traveling opens the door to creating <span className='hightlight'> memories</span></h1>
+                <p>
+                   Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                   Ullam ipsum nobis asperiores soluta voluptas quas voluptates.
+                   Molestiae tempora dignissimos, animi praesentium molestias perferendis
+                   porro expedita delectus. Soluta natus porro.
+                </p>
+             </div>
+          </Col>
+
+          <Col lg='2'>
+             <div className="hero__img-box">
+                <img src={heroImg} alt="" />
+             </div>
+          </Col>
+          <Col lg='2'>
+             <div className="hero__img-box hero__video-box mt-4">
+                <video src={heroVideo} alt="" controls />
+             </div>
+          </Col>
+          <Col lg='2'>
+             <div className="hero__img-box mt-5">
+                <img src={heroImg02} alt="" />
+             </div>
+          </Col>
+
+         <Bar />
+       </Row>
+    </Container>
+ </section>
   );
 }
 
